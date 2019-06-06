9e4af1e13aef26246d43a6a5060ee5b3d400df90
Maksim Murygin <mail@muryginm.com>
2019-06-06 12:39:40 -0700

    update

--- scripts/containers.sh
+++ scripts/containers.sh
@@ -1,13 +1,15 @@
+PROJECT_ID=`gcloud config get-value project`
+
 gcloud auth configure-docker
 
 docker build -t gcloud-demo .
-docker tag gcloud-demo gcr.io/$(gcloud config get-value project)/gcloud-demo:v2
-docker push gcr.io/$(gcloud config get-value project)/gcloud-demo:v2
+docker tag gcloud-demo gcr.io/$PROJECT_ID/gcloud-demo:v2
+docker push gcr.io/$PROJECT_ID/gcloud-demo:v2
 
-gcloud builds submit --tag gcr.io/$(gcloud config get-value project)/gcloud-demo:v2
 
 # spin up another virtual machine
 gcloud compute instances create container-demo --machine-type=n1-standard-1 --tags=http-server --image=workstation-with-docker --boot-disk-size=20GB --zone=us-central1-a
 gcloud compute ssh container-demo
-docker run -d -p 80:5000 gcr.io/$(gcloud config get-value project)/gcloud-demo:v2
+docker run -d -p 80:5000 gcr.io/$PROJECT_ID/gcloud-demo:v2
 
+gcloud builds submit --tag gcr.io/$PROJECT_ID/gcloud-demo:v3
