d8f16c705953989b3455d82e43c646e4606ff643
Maksim Murygin <mail@muryginm.com>
2019-06-06 12:16:22 -0700

    update container demo

--- scripts/containers.sh
+++ scripts/containers.sh
@@ -4,6 +4,8 @@ docker build -t gcloud-demo .
 docker tag gcloud-demo gcr.io/$(gcloud config get-value project)/gcloud-demo:v2
 docker push gcr.io/$(gcloud config get-value project)/gcloud-demo:v2
 
+gcloud builds submit --tag gcr.io/$(gcloud config get-value project)/gcloud-demo:v2
+
 # spin up another virtual machine
 gcloud compute instances create container-demo --machine-type=n1-standard-1 --tags=http-server --image=workstation-with-docker --boot-disk-size=20GB --zone=us-central1-a
 gcloud compute ssh container-demo
